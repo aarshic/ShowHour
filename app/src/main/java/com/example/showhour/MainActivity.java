b5cbcd3598c31b5fbc05fca8662b109f23caa71e
aarshic <aarshi.1998@gmail.com>
2021-06-16 17:37:33 +0530

    Initial commit

---
+++ app/src/main/java/com/example/showhour/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.showhour;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
